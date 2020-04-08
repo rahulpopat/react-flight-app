0a673d231e842a15dcb1d7d0a6ba39d440eb27a3
Rahul Popat <poprahul@amazon.com>
2020-04-08 11:28:02 -0400

    added logo

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { css } from 'glamor'
+import logo from './assets/images/logo.png';
 
 export default class Nav extends React.Component {
   render() {
     return (
       <div {...css(styles.container)}>
-        <h1 {...css(styles.heading)}>Booking Application</h1>
-        {/* <Link to='/' {...css(styles.link)}></Link> */}
-        <Link to='/flights' {...css(styles.link)}>Flights</Link>
-        {/* <Link to='/search' {...css(styles.link)}>Search Flights</Link> */}
-        <Link to='/flightFilter' {...css(styles.link)}>Search Flights</Link>
+        {/* <a class="navbar-brand" href="#"><img src={logo} alt="logo image" /></a> */}
+          <img src={logo} alt="logo image" height="50" width="70"/>
+          <h1 {...css(styles.heading)}>Octank Travels</h1>
+          {/* <h1 {...css(styles.heading)}>&nbsp;&nbsp;Octank Travels</h1> */}
+          {/* <Link to='/' {...css(styles.link)}></Link> */}
+          <Link to='/flights' {...css(styles.link)}>Flights</Link>
+          {/* <Link to='/search' {...css(styles.link)}>Search Flights</Link> */}
+          <Link to='/flightFilter' {...css(styles.link)}>Search Flights</Link>
       </div>
     )
   }
@@ -27,12 +31,14 @@ const styles = {
   },
   container: {
     display: 'flex',
+    // backgroundColor: '#ff6000',
     backgroundColor: '#0068c3',
     padding: '0px 30px',
     alignItems: 'center'
   },
   heading: {
     color: 'white',
-    paddingRight: 20
+    paddingRight: 20,
+    marginLeft: 20
   }
 }
