7d735732331f0069afc6f388806a8adede96a641
Rahul Popat <poprahul@amazon.com>
2020-04-15 15:08:14 -0400

    update api key

--- src/AppSync.js
+++ src/AppSync.js
@@ -2,5 +2,5 @@
     "graphqlEndpoint": "https://5gbsraqwdrerzgjg5zbdl3tadu.appsync-api.us-east-1.amazonaws.com/graphql",
     "region": "us-east-1",
     "authenticationType": "API_KEY",
-    "apiKey": "da2-awu3vtuskzburn37ha3t62xcjq"
+    "apiKey": "da2-adle3pwoljgubjy6r2npvkxufq"
   }
